1e789d8445c5e589bed2829b3e99f26e2ae349b3
DucPhan18 <99118919+DucPhan18@users.noreply.github.com>
2023-11-23 15:25:40 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e,f;
+    cout << "Bang gia tri:"  << endl;
+    cout << "f(x)=ax^2+bx+c"<<endl;
+    cout << "Nhap a="; cin >> a;
+    cout << "Nhap b="; cin >> b;
+    cout << "Nhap c="; cin >> c;
+    cout << "------------------" <<endl;
+    cout << "Pham vi bang"<< endl;
+    cout << "Bat đau  : " ; cin >> d;
+    cout << "Ket thuc : " ; cin >> e;
+    cout << "Buoc     : " ; cin >> f;
+    
+    return 0;
+}
